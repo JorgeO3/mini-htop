d317889cf30f534a2238daae3e9acc13f0f23c94
Jorge Osorio <joheosmo@gmail.com>
2023-08-15 00:23:48 -0500

    feat(ui): implement the memoy usage draw of the second block

--- src/ui.rs
+++ src/ui.rs
@@ -22,8 +22,8 @@ pub fn draw<B: Backend>(f: &mut Frame<B>, sys: &mut SystemResources) {
         .split(f.size());
 
     draw_cpu_graph(f, sys, chunks[0]);
-    draw_cpu_graph(f, sys, chunks[1]);
-    draw_cpu_graph(f, sys, chunks[2]);
+    draw_system_stats(f, sys, chunks[1]);
+    draw_network_and_processes(f, sys, chunks[2]);
 }
 
 // first block
@@ -60,7 +60,63 @@ pub fn draw_system_stats<B>(f: &mut Frame<B>, sys: &mut SystemResources, area: R
 where
     B: Backend,
 {
+    let chuncks = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([Constraint::Ratio(1, 3), Constraint::Ratio(2, 3)].as_ref())
+        .split(area);
+
+    let vertical_chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([Constraint::Ratio(1, 5), Constraint::Ratio(1, 5)])
+        .split(chuncks[0]);
+
+    draw_memory_usage(f, chuncks[1]);
+    draw_disk_usage(f, vertical_chunks[0]);
+    draw_components_temps(f, vertical_chunks[1]);
 }
+
+// second block
+pub fn draw_memory_usage<B>(f: &mut Frame<B>, area: Rect)
+where
+    B: Backend,
+{
+    let items = [1, 2, 3]
+        .iter()
+        .map(|c| {
+            let cells = vec![
+                Cell::from(Span::raw(format!("{c:?}: "))),
+                Cell::from(Span::styled(
+                    "Foreground",
+                    Style::default().fg(Color::Rgb(255, 255, 255)),
+                )),
+            ];
+            Row::new(cells)
+        })
+        .collect::<Vec<_>>();
+
+    let table = Table::new(items)
+        .block(Block::default().title("Colors").borders(Borders::ALL))
+        .widths(&[
+            Constraint::Ratio(1, 3),
+            Constraint::Ratio(1, 3),
+            Constraint::Ratio(1, 3),
+        ]);
+    f.render_widget(table, area);
+}
+pub fn draw_disk_usage<B>(f: &mut Frame<B>, area: Rect)
+where
+    B: Backend,
+{
+}
+pub fn draw_components_temps<B>(f: &mut Frame<B>, area: Rect)
+where
+    B: Backend,
+{
+}
+
+// network and process
+pub fn draw_network_usage() {}
+pub fn draw_list_process() {}
 
 // network and process
 pub fn draw_network_and_processes<B>(f: &mut Frame<B>, sys: &mut SystemResources, area: Rect)
@@ -68,12 +124,3 @@ where
     B: Backend,
 {
 }
-
-// second block
-pub fn draw_disk_usage() {}
-pub fn draw_memory_usage() {}
-pub fn draw_components_temps() {}
-
-// network and process
-pub fn draw_network_usage() {}
-pub fn draw_list_process() {}
